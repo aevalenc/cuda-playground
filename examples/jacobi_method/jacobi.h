b103eea637091f94d6bbac42a8efbec3d2ad6d38
Alejandro Valencia <alejandroedgardo.valencia@stellantis.com>
2025-07-23 14:53:55 +0200

    Implement Jacobi kernel with tiling

--- examples/jacobi_method/jacobi.h
+++ examples/jacobi_method/jacobi.h
@@ -9,10 +9,11 @@
 
 #include <cstdint>
 
-__device__ constexpr std::int32_t kBlockSize = 16;
+__device__ constexpr std::int32_t kBlockSize = 2;
 
 extern "C" {
 __global__ void JacobiSolveGPU(const double* A, const double* b, double* x0, double* x, std::int32_t N);
+__global__ void JacobiSolveWithTilingGPU(const double* A, const double* b, double* x0, double* x, std::int32_t N);
 }
 
 #endif  // EXAMPLES_JACOBI_METHOD_JACOBI_H
